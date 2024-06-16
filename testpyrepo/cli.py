750fb3d1fef24c939411946723a7805fcf8cb7d9
jlgerber <jlgerber@users.noreply.github.com>
2024-06-16 15:26:27 +0000

    ✅ Ready to clone and code.

--- testpyrepo/cli.py
+++ testpyrepo/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for testpyrepo project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m testpyrepo` and `$ testpyrepo `.
 
     This is your program's entry point.
 
